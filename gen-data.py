aa586b2ff216b5e7436c541d7a35750e0801ce70
Varun Jindal <vjindal0112@gmail.com>
2022-12-27 15:00:54 -0500

    smaller files created

--- gen-data.py
+++ gen-data.py
@@ -25,37 +25,38 @@ EDITIONS = [1, 2, 3]
 
 # CREATE AUTHORS
 # open the file create-data.sql to write to
-# with open('create-books-1.sql', 'w') as f:
-# write fake data to the file given the schema in init-db.sql
-# f.write("INSERT INTO authors (author_name, country)\n")
-# f.write("VALUES ")
-# for x in range(10000000):
-#   f.write("('" + random.choice(NAMES) + "', '" + random.choice(COUNTRIES) + "')")
-#   if x != 9999999:
-#     f.write(", ")
-# f.write(";\n")
-# f.write("\n")
+# with open('create-authors-smaller.sql', 'w') as f:
+# # write fake data to the file given the schema in init-db.sql
+#   f.write("INSERT INTO authors (author_name, country)\n")
+#   f.write("VALUES ")
+#   for x in range(100000):
+#     f.write("('" + random.choice(NAMES) + "', '" + random.choice(COUNTRIES) + "')")
+#     if x != 99999:
+#       f.write(",\n")
+#   f.write(";")
+#   f.write("\n")
 
 # CREATE BOOKS
 # for k in range(8):
-#   with open('create-books-' + str(k) + '.sql', 'w') as f:
-#     f.write("INSERT INTO books (author_id, price, edition)\n")
-#     f.write("VALUES ")
-#     for x in range(2500000 * k, 2500000 * (k + 1)):
-#       for y in range(random.randint(2, 10)):
-#         f.write("('" + str(x) + "', '" + str(random.choice(PRICES)) +
-#                 "', '" + str(random.choice(EDITIONS)) + "')")
-#         if x != ((2500000 * (k + 1)) - 1) or y != 9:
-#           f.write(", ")
-#     f.write(";")
+# with open('create-books-smaller.sql', 'w') as f:
+#   f.write("INSERT INTO books (author_id, price, edition)\n")
+#   f.write("VALUES ")
+#   for x in range(0, 25000):
+#     k = random.randint(2, 10)
+#     for y in range(k):
+#       f.write("('" + str(x) + "', '" + str(random.choice(PRICES)) +
+#               "', '" + str(random.choice(EDITIONS)) + "')")
+#       if x != 24999 or y != k-1:
+#         f.write(", ")
+#   f.write(";")
 
 # CREATE POINTS
-with open('create-points.sql', 'w') as f:
+with open('create-points-smaller.sql', 'w') as f:
   f.write("INSERT INTO points (loc)\n")
   f.write("VALUES ")
-  for x in range(1000000):
+  for x in range(100000):
     f.write("(point(" + str(random.randint(0, 10000)) +
             ", " + str(random.randint(0, 10000)) + "))")
-    if x != 999999:
+    if x != 99999:
       f.write(",\n")
   f.write(";")
